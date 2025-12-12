c9487622d5c80a9448862bb3d76e0a692a7aab2d
Eduardo Bandeira <eduardo.bandeira.eng@outlook.com>
2025-12-12 01:23:46 -0300

    New README

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+vert=$(glslang vShader.vert)
+frag=$(glslang fShader.frag)
+geom=$(glslang gShader.geom)
+
+if [[ "$vert" = "" && "$frag" = ""  && "$geom" = ""  ]]; then
+  ./build.pl && ./build/final
+else
+  echo $vert
+  echo $frag
+  echo $geom
+fi
